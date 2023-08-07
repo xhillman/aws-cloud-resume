d778c2310785230cfa3e7bb3d43fb81c20a9e713
Xavier Hillman <xhillman13@gmail.com>
2023-08-07 18:48:14 -0400

    add nav functionality to hero icons

--- script.js
+++ script.js
@@ -7,13 +7,16 @@ const homeNav = document.querySelector('.home-nav');
 const aboutNav = document.querySelector('.about-nav');
 const projectsNav = document.querySelector('.projects-nav');
 const contactNav = document.querySelector('.contact-nav');
+const aboutIcon = document.querySelector('.about-icon');
+const projectsIcon = document.querySelector('.projects-icon');
+const contactIcon = document.querySelector('.contact-icon');
 
 const setPage = (page) => {
   homePage.style.display = 'none';
   aboutPage.style.display = 'none';
   projectsPage.style.display = 'none';
   contactPage.style.display = 'none';
-  page.style.display = 'block';
+  page.style.display = 'flex';
 }
 
 const setNav = (nav) => {
@@ -22,6 +25,26 @@ const setNav = (nav) => {
   projectsNav.classList.remove('selected-nav');
   contactNav.classList.remove('selected-nav');
   nav.classList.add('selected-nav');
+}
+
+const setPageFromIcon = (icon) => {
+  switch (icon) {
+    case aboutIcon:
+      setPage(aboutPage);
+      setNav(aboutNav);
+      break;
+    case projectsIcon:
+      setPage(projectsPage);
+      setNav(projectsNav);
+      break;
+    case contactIcon:
+      setPage(contactPage);
+      setNav(contactNav);
+      break;
+    default:
+      setPage(homePage);
+      setNav(homeNav);
+  }
 }
 
 //To Do: save current page
@@ -49,4 +72,16 @@ contactNav.addEventListener('click', () => {
   setPage(contactPage);
   setNav(contactNav);
   currentPage = contactPage;
+});
+
+aboutIcon.addEventListener('click', () => {
+  setPageFromIcon(aboutIcon);
+});
+
+projectsIcon.addEventListener('click', () => {
+  setPageFromIcon(projectsIcon);
+});
+
+contactIcon.addEventListener('click', () => {
+  setPageFromIcon(contactIcon);
 });
