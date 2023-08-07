7271e708846cb6d465149d3227d96d6f435afa8f
Xavier Hillman <xhillman13@gmail.com>
2023-08-07 16:22:03 -0400

    refactor page layout and add page navigation

---
+++ script.js
@@ -0,0 +1,37 @@
+let currentPage;
+const homePage = document.querySelector('.home');
+const aboutPage = document.querySelector('.about');
+const projectsPage = document.querySelector('.projects');
+const contactPage = document.querySelector('.contact');
+const homeNav = document.querySelector('.home-nav');
+const aboutNav = document.querySelector('.about-nav');
+const projectsNav = document.querySelector('.projects-nav');
+const contactNav = document.querySelector('.contact-nav');
+
+const setPage = (page) => {
+  homePage.style.display = 'none';
+  aboutPage.style.display = 'none';
+  projectsPage.style.display = 'none';
+  contactPage.style.display = 'none';
+  page.style.display = 'block';
+}
+
+homeNav.addEventListener('click', () => {
+  setPage(homePage);
+  currentPage = homePage;
+});
+
+aboutNav.addEventListener('click', () => {
+  setPage(aboutPage);
+  currentPage = aboutPage;
+});
+
+projectsNav.addEventListener('click', () => {
+  setPage(projectsPage);
+  currentPage = projectsPage;
+});
+
+contactNav.addEventListener('click', () => {
+  setPage(contactPage);
+  currentPage = contactPage;
+});
